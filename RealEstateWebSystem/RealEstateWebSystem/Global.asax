8ae4826bf7f54d7f1ca72b58115e852ecf890a1c
vasilev25 <pvdev95@gmail.com>
2017-02-19 18:21:11 +0200

    First proj

---
+++ RealEstateWebSystem/RealEstateWebSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealEstateWebSystem.MvcApplication" Language="C#" %>
---
+++ RealEstateWebSystem/RealEstateWebSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealEstateWebSystem.Startup))]
+namespace RealEstateWebSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
